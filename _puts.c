5b8b56a91f63f50edbaeaa5d8960a86e51e90398
VYMNN47 <ayman.belahsen.4@gmail.com>
2023-10-14 21:07:45 +0100

    Create _puts.c
    Prints a string and returns the length

---
+++ _puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string and returns the length
+ * @str: The string to be printed
+ *
+ * Return: returns the length of the printed string
+*/
+
+int _puts(char *str)
+{
+        int strlen = 0;
+
+        while (*str != '\0')
+        {
+                strlen += _putchar(*str);
+                str++;
+        }
+
+        return (strlen);
+}
