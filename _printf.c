6b94746a8c2fe480b211a72b86b9fee010f7b8eb
VYMNN47 <ayman.belahsen.4@gmail.com>
2023-10-16 18:29:45 +0100

    Making changes in _printf

--- _printf.c
+++ _printf.c
@@ -9,44 +9,15 @@
 
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i = 0, len = 0;
+        va_list args;
+        int len = 0;
 
-	va_start(args, format);
-	if (!format)
-		return (-1);
-	while (format && format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == '%')
-				len += _putchar('%');
-			else if (format[i] == '\0')
-				return (-1);
+        va_start(args, format);
+        if (!format)
+                return (-1);
 
-			else
-				switch (format[i])
-				{
-					case 'c':
-						len += _putchar(va_arg(args, int));
-						break;
-					case 's':
-						len += _puts(va_arg(args, char *));
-						break;
-					case 'd':
-					case 'i':
-						len += intToStr(va_arg(args, int));
-						break;
-					default:
-						len += _putchar('%') + _putchar(format[i]);
-						break;
-				}
-		}
-		else
-			len += _putchar(format[i]);
-		i++;
-	}
-	va_end(args);
-	return (len);
+        len = processFormatString(format, args);
+
+        va_end(args);
+        return len;
 }
