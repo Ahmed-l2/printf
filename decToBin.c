24f1b658a67ff30bba11d3809b2e681d5e8bc23e
VYMNN47 <ayman.belahsen.4@gmail.com>
2023-10-16 18:31:12 +0100

    Converts an interger to binary

---
+++ decToBin.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * decToBin - converts integer to binary
+ * @num: given integer
+ * Return: length
+ */
+
+int decToBin(unsigned int num)
+{
+    char binary[32];
+    int i = 0;
+    int j, len;
+
+    while (num > 0) 
+    {
+        binary[i++] = '0' + (num % 2);
+        num /= 2;
+    }
+    
+    for (j = i - 1; j >= 0; j--) 
+    {
+        _putchar(binary[j]);
+        len++;
+    }
+    return (len);
+}
