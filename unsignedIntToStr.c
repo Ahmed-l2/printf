984e13f59f430ed5ed4cabc838fd2dc31cb90aec
Ahmed <ahmed_l2@yahoo.com>
2023-10-16 16:24:17 -0700

    added function to handle %u

---
+++ unsignedIntToStr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * unsignedIntToStr - Converts an integer to a string and prints it
+ * @num: The integer to convert and print
+ *
+ * Return: The length of the printed string
+ */
+
+unsigned int unsignedIntToStr(unsigned int num)
+{
+	int length = 0;
+	int digits[14];
+	int i;
+
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	else
+	{
+		while (num > 0)
+		{
+			digits[length] = num % 10;
+			num /= 10;
+			length++;
+		}
+
+		for (i = length - 1; i >= 0; i--)
+		{
+			_putchar('0' + digits[i]);
+		}
+	}
+	return (length);
+}
