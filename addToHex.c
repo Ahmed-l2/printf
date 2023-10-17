753d64849aa757b900a6fd4c4d180a695f3c1283
VYMNN47 <ayman.belahsen.4@gmail.com>
2023-10-17 22:28:36 +0100

    Convert a pointer to a hexadecimal representation

---
+++ addToHex.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+int addToHex(void *ptr)
+{
+        unsigned long int num = (unsigned long int)ptr;
+        int len = 0;
+        char hexBuffer[20];
+        char reversedBuffer[20];
+        int remainder;
+        int i;
+
+        if (num == 0)
+        {
+                _putchar('0');
+                return (1);
+        }
+        
+
+        while (num > 0)
+        {
+                remainder = num % 16;
+                if (remainder < 10)
+                {
+                        hexBuffer[len] = '0' + remainder;
+                }
+                else
+                {
+                        hexBuffer[len] = 'a' + (remainder - 10);
+                }
+                num /= 16;
+                len++;
+        }
+
+        for (i = 0; i < len; i++)
+        {
+                reversedBuffer[i] = hexBuffer[len - i - 1];
+        }
+
+
+        return (_printf("0x%s", reversedBuffer));
+}
