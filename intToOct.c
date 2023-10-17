7610a71386bd537e29c42378888bce9c413def19
Ahmed <ahmed_l2@yahoo.com>
2023-10-16 17:28:10 -0700

    function to handle %o conversions

---
+++ intToOct.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * intToOct - function that converts an int to its octal value
+ * @num: given integer
+ * Return: returns the length
+ */
+
+int intToOct(int num)
+{
+	char octalNumber[100];
+	int i = 0, len = 0, j;
+
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+
+	while (num > 0)
+	{
+		octalNumber[i] = '0' +  (num % 8);
+		num /= 8;
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(octalNumber[j]);
+		len++;
+	}
+	return (len);
+}
