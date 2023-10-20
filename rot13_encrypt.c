321437d7f078dd03cf78467fb172fec1145b8a5d
Ahmed <ahmed_l2@yahoo.com>
2023-10-20 11:48:31 -0700

    fixed betty

--- rot13_encrypt.c
+++ rot13_encrypt.c
@@ -9,20 +9,20 @@
 
 int rot13_encrypt(char *str)
 {
-    if (str == NULL)
-        return 0;
+	if (str == NULL)
+		return (0);
 
-    int length = 0;
-    while (*str)
-    {
-        if ((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z'))
-        {
-            char base = (*str >= 'A' && *str <= 'Z') ? 'A' : 'a';
-            *str = (*str - base + 13) % 26 + base;
-        }
-        putchar(*str);
-        str++;
-        length++;
-    }
-    return length;
+	int length = 0;
+	while (*str)
+	{
+		if ((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z'))
+		{
+			char base = (*str >= 'A' && *str <= 'Z') ? 'A' : 'a';
+			*str = (*str - base + 13) % 26 + base;
+		}
+		putchar(*str);
+		str++;
+		length++;
+	}
+	return length;
 }
